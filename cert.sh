9cec48052442c5d631cd2159037d276e060161ea
Sehyun Hwang <hwanghyun3@gmail.com>
2021-11-14 18:08:56 +0900

    Minor mods

--- cert.sh
+++ cert.sh
@@ -1,6 +1,5 @@
 set -e
 
-sudo docker run -it --rm -v cert:/etc/letsencrypt certbot/dns-route53 certificates
 #sudo docker run -it --rm -v cert:/etc/letsencrypt certbot/dns-route53 certonly -d '*.hwangsehyun.com' -d hwangsehyun.com --dns-route53
 
 [ -z "$1" ] || SSH="ssh $@"
@@ -11,6 +10,7 @@ echo $CMD
 
 echo 'cat /cert/live/hwangsehyun.com/{fullchain.pem,privkey.pem}' \
 | sudo docker run -i --rm -w /cert -v cert:/cert bash \
-| $CMD run -i --rm -v cert:/cert alpine tee /cert/cert.pem
+| $CMD run -i --rm -v cert:/cert \
+alpine tee /cert/cert.pem
 
-$CMD run -i --rm -v cert:/cert alpine ls -la /cert
+$CMD run -i --rm -v cert:/cert --security-opt=label=disable alpine ls -la /cert
