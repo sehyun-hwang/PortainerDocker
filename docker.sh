f1e9103527641b5da29b9af22027a34d0f862d77
Sehyun Hwang <hwanghyun3@gmail.com>
2024-07-21 07:22:36 +0000

    wip docker compose stack

--- docker.sh
+++ docker.sh
@@ -18,267 +18,238 @@ echo Container name: $1
 # done
 
 function Password {
-    if [ ! -z $REPLY ]; then
-        read -p Password:
-    fi
+	if [ ! -z $REPLY ]; then
+		read -p Password:
+	fi
 }
 
 ARGS=$2
 
 case $1 in
-    dev)
-    docker run -it --rm --pod nginx-pod --name dev \
-        -w /mnt -v node_modules:/mnt/node_modules -v $PWD:/mnt \
-        -v ~/.bash_history:/root/.bash_history:z \
-        -v ~/.cache/yarn/v6:/usr/local/share/.cache/yarn/v6 \
-        --security-opt label=disable node:alpine sh
-    ;;
+dev)
+	docker run -it --rm --pod nginx-pod --name dev \
+		-w /mnt -v node_modules:/mnt/node_modules -v $PWD:/mnt \
+		-v ~/.cache/yarn/v6:/usr/local/share/.cache/yarn/v6 \
+		--security-opt label=disable node:alpine sh
+	;;
 
-\
-    nginx)
-    #docker start pgadmin portainer registry-browser redisinsight
+hugo)
+	docker run -it --rm --pod nginx-pod --name dev \
+		-w /mnt -v node_modules:/mnt/node_modules -v $PWD:/mnt \
+		-v ~/.cache/yarn/v6:/usr/local/share/.cache/yarn/v6 \
+		--security-opt label=disable node:alpine sh
+	;;	
 
-    docker pod ls -f=status=degraded -q | grep nginx-pod && docker pod rm nginx-pod
-    docker pod create --name nginx-pod --net network \
-        -p 80:80 -p 443:443/tcp -p 443:443/udp -p 5432:5432 -p 6379:6379 || true
 
-    docker run --name nginx -d $ARGS \
-        --restart unless-stopped \
-        --pod nginx-pod \
-        -e NGINX_ENTRYPOINT_LOCAL_RESOLVERS=true \
-        -e NGINX_ENVSUBST_TEMPLATE_DIR=/etc/nginx/conf \
-        -v /etc/localtime:/etc/localtime:ro \
-        -v cert:/cert:ro \
-        -v /mnt/Docker/nginx/etc-hosts.sh:/docker-entrypoint.d/etc-hosts.sh:ro \
-        -v /mnt/Docker/nginx:/etc/nginx/conf:ro \
-        -v /mnt:/mnt:ro -v /volatile/src:/volatile:ro \
-        --security-opt label=disable \
-        --log-opt max-size=100m \
-        nginx:alpine \
-        nginx -g 'daemon off;' -c /etc/nginx/conf/nginx.conf
-    ;;
+nginx)
+	# docker compose start pgadmin portainer registry-browser redisinsight
+	NETWORK=$(docker compose config | awk '/networks:/ { getline; if ($0 ~ /default:$/) { getline; print $2 }}')
 
-\
-    redisinsight)
-    docker run --name redisinsight -d $ARGS \
-        --net network \
-        -v redisinsight:/db \
-        --security-opt label=disable -u root \
-        -e RIPROXYPATH=/redisinsight -e RIPROXYENABLE=True -e RITRUSTEDORIGINS=https://proxy.hwangsehyun.com \
-        --entrypoint python \
-        redislabs/redisinsight \
-        entry.pyc
+	docker pod ls -f=status=degraded -q | grep nginx-pod && docker pod rm nginx-pod
+	docker pod create --name nginx-pod --net $NETWORK \
+		-p 80:80 -p 443:443/tcp -p 443:443/udp || true
 
-    docker kill -s HUP nginx
-    ;;
+	docker run --name nginx -d $ARGS \
+		--restart unless-stopped \
+		--pod nginx-pod \
+		-e NGINX_ENTRYPOINT_LOCAL_RESOLVERS=true \
+		-e NGINX_ENVSUBST_TEMPLATE_DIR=/etc/nginx/conf \
+		-v /etc/localtime:/etc/localtime:ro \
+		-v cert:/cert:ro \
+		-v /mnt/Docker/nginx/etc-hosts.sh:/docker-entrypoint.d/etc-hosts.sh:ro \
+		-v /mnt/Docker/nginx:/etc/nginx/conf:ro \
+		-v /mnt:/mnt:ro \
+		--security-opt label=disable \
+		--log-opt max-size=100m \
+		nginx:alpine \
+		nginx -g 'daemon off;' -c /etc/nginx/conf/nginx.conf
+	;;
 
-\
-    redisinsight-2)
-    docker run -d --name redisinsight-2 $ARGS \
-        --net network \
-        -v /mnt/Docker/redis-insight-2.sh:/entrypoint.sh:ro \
-        -v //mnt/Docker/nginx/redisinsight2.html:/opt/redis-stack/share/redisinsight/ui/dist/index.html:ro \
-        -v /mnt/Docker/redisinsight-2:/data \
-        redis/redis-stack
+# redisinsight)
+# 	docker run --name redisinsight -d $ARGS \
+# 		--net network \
+# 		-v redisinsight:/db \
+# 		--security-opt label=disable -u root \
+# 		-e RIPROXYPATH=/redisinsight -e RIPROXYENABLE=True -e RITRUSTEDORIGINS=https://proxy.hwangsehyun.com \
+# 		--entrypoint python \
+# 		redislabs/redisinsight \
+# 		entry.pyc
 
-    docker kill -s HUP nginx
-    ;;
+# 	docker kill -s HUP nginx
+# 	;;
 
-\
-    portainer)
-    docker run -d --name portainer $ARGS \
-        --net network \
-        --security-opt label=disable \
-        -v /mnt/Docker/portainer:/data \
-        portainer/portainer-ce:alpine
+# redisinsight-2)
+# 	docker run -d --name redisinsight-2 $ARGS \
+# 		--net network \
+# 		-v /mnt/Docker/redis-insight-2.sh:/entrypoint.sh:ro \
+# 		-v //mnt/Docker/nginx/redisinsight2.html:/opt/redis-stack/share/redisinsight/ui/dist/index.html:ro \
+# 		-v /mnt/Docker/redisinsight-2:/data \
+# 		redis/redis-stack
 
-    docker kill -s HUP nginx
-    ;;
+# 	docker kill -s HUP nginx
+# 	;;
 
-\
-    portainer-agent)
-    DOCKER=/var/run/docker.sock
-    SRC=/run/user/1000/podman/podman.sock
-    docker --version | grep podman || SRC=$DOCKER
+# portainer)
+# 	docker run -d --name portainer $ARGS \
+# 		--net network \
+# 		--security-opt label=disable \
+# 		-v /mnt/Docker/portainer:/data \
+# 		portainer/portainer-ce:alpine
 
-    echo Mounting $SRC
+# 	docker kill -s HUP nginx
+# 	;;
 
-    docker run -d --name portainer-agent $ARGS \
-        --restart unless-stopped \
-        --net network -p 9001:9001 \
-        -v $SRC:$DOCKER \
-        --security-opt label=disable \
-        portainer/agent
-    ;;
+portainer-agent)
+	DOCKER=/var/run/docker.sock
+	SRC=/run/user/1000/podman/podman.sock
+	docker --version | grep podman || SRC=$DOCKER
 
-\
-    pgadmin)
-    docker run --name pgadmin -d $ARGS \
-        --net network --restart unless-stopped \
-        -v /mnt/Docker/pgadmin-servers.json:/pgadmin4/servers.json \
-        --security-opt label=disable \
-        -e SCRIPT_NAME=/pgadmin \
-        -e PGADMIN_DEFAULT_EMAIL=hwanghyun3@gmail.com -e PGADMIN_DEFAULT_PASSWORD=gTff8ULka4NuJsV \
-        dpage/pgadmin4
+	echo Mounting $SRC
 
-    docker kill -s HUP nginx
-    ;;
+	docker run -d --name portainer-agent $ARGS \
+		--restart unless-stopped \
+		--net network -p 9001:9001 \
+		-v $SRC:$DOCKER \
+		--security-opt label=disable \
+		portainer/agent
+	;;
 
-\
-    stream)
-    docker run --name stream -d $ARGS \
-        --net network -p 8081:80 \
-        shurco/nginx-push-stream-module
+# pgadmin)
+# 	docker run --name pgadmin -d $ARGS \
+# 		--net network --restart unless-stopped \
+# 		-v /mnt/Docker/pgadmin-servers.json:/pgadmin4/servers.json \
+# 		--security-opt label=disable \
+# 		-e SCRIPT_NAME=/pgadmin \
+# 		-e PGADMIN_DEFAULT_EMAIL=hwanghyun3@gmail.com -e PGADMIN_DEFAULT_PASSWORD=gTff8ULka4NuJsV \
+# 		dpage/pgadmin4
 
-    docker kill -s HUP nginx
-    ;;
+# 	docker kill -s HUP nginx
+# 	;;
 
-\
-    registry-browser)
-    docker run --name registry-browser -d $ARGS \
-        --net network \
-        -e DOCKER_REGISTRY_URL=https://nextlab.hwangsehyun.com:41443 \
-        -e SCRIPT_NAME=/registrybrowser \
-        -e RAILS_RELATIVE_URL_ROOT=/registrybrowser \
-        klausmeyer/docker-registry-browser
+# stream)
+# 	docker run --name stream -d $ARGS \
+# 		--net network -p 8081:80 \
+# 		shurco/nginx-push-stream-module
 
-    docker kill -s HUP nginx
-    ;;
+# 	docker kill -s HUP nginx
+# 	;;
 
-\
-    container-stats)
-    docker run --name container-stats -d $ARGS \
-        --net network \
-        -v /run/user/1000/podman/podman.sock:/var/run/docker.sock:ro \
-        --volume=stats:/opt/docker-stats/db --security-opt label=disable \
-        virtualzone/docker-container-stats
-    ;;
+# registry-browser)
+# 	docker run --name registry-browser -d $ARGS \
+# 		--net network \
+# 		-e DOCKER_REGISTRY_URL=https://nextlab.hwangsehyun.com:41443 \
+# 		-e SCRIPT_NAME=/registrybrowser \
+# 		-e RAILS_RELATIVE_URL_ROOT=/registrybrowser \
+# 		-e SECRET_KEY_BASE=$(openssl rand -hex 64) \
+# 		klausmeyer/docker-registry-browser
 
-\
-    rtsp-server)
-    docker run -d --name rtsp-server \
-        -e RTSP_PROTOCOLS=tcp \
-        -p 8554:8554 -p 1935:1935 \
-        aler9/rtsp-simple-server
-    ;;
+# 	docker kill -s HUP nginx
+# 	;;
 
-\
-    cctv)
-    docker run --name cctv -d \
-        linuxserver/ffmpeg \
-        -re -i rtsp://root:root@192.168.0.133/cam0_0 \
-        -c copy -map 0 -f rtp_mpegts -fec prompeg=l=5:d=20 \
-        rtp://18.138.27.6:5000
-    ;;
+# container-stats)
+# 	docker run --name container-stats -d $ARGS \
+# 		--net network \
+# 		-v /run/user/1000/podman/podman.sock:/var/run/docker.sock:ro \
+# 		--volume=stats:/opt/docker-stats/db --security-opt label=disable \
+# 		virtualzone/docker-container-stats
+# 	;;
 
-\
-    s3)
-    docker run -d --name s3 \
-        --net network \
-        --restart unless-stopped \
-        -w /mnt \
-        -v /Volumes/dev/node_modules:/mnt/node_modules:ro -v /Volumes/dev/package.json:/package.json:ro \
-        -v /Volumes/dev/S3:/mnt/data \
-        node:alpine \
-        node node_modules/s3rver/bin/s3rver.js -a 0.0.0.0 -d data \
-        --no-vhost-buckets --configure-bucket hwangsehyun --configure-bucket nextlab
-    ;;
+s3)
+	docker run -d --name s3 \
+		--net network \
+		--restart unless-stopped \
+		-w /mnt \
+		-v /Volumes/dev/node_modules:/mnt/node_modules:ro -v /Volumes/dev/package.json:/package.json:ro \
+		-v /Volumes/dev/S3:/mnt/data \
+		node:alpine \
+		node node_modules/s3rver/bin/s3rver.js -a 0.0.0.0 -d data \
+		--no-vhost-buckets --configure-bucket hwangsehyun --configure-bucket nextlab
+	;;
 
-\
-    php)
-    docker run --name php -d \
-        --restart unless-stopped \
-        --net network \
-        -v php-aws:/mnt/ptais/php-aws -v /mnt/ptais:/mnt/ptais \
-        bitnami/php-fpm
-    ;;
+php)
+	docker run --name php -d \
+		--restart unless-stopped \
+		--net network \
+		-v php-aws:/mnt/ptais/php-aws -v /mnt/ptais:/mnt/ptais \
+		bitnami/php-fpm
+	;;
 
-\
-    parallels-backup)
-    docker run --name parallels-backup -it \
-        --restart unless-stopped \
-        -v /Volumes/Data/Parallels:/src:ro -v /Volumes/dev/Parallels:/dsc \
-        alpine sh -c 'wget https://www.hwangsehyun.com/Docker/parallels-backup.sh -O script.sh; sh script.sh'
-    ;;
+parallels-backup)
+	docker run --name parallels-backup -it \
+		--restart unless-stopped \
+		-v /Volumes/Data/Parallels:/src:ro -v /Volumes/dev/Parallels:/dsc \
+		alpine sh -c 'wget https://www.hwangsehyun.com/Docker/parallels-backup.sh -O script.sh; sh script.sh'
+	;;
 
-\
-    meshlab)
-    docker run -d --name meshlab \
-        --privileged \
-        --restart unless-stopped \
-        --net network \
-        meshlab
-    ;;
+meshlab)
+	docker run -d --name meshlab \
+		--privileged \
+		--restart unless-stopped \
+		--net network \
+		meshlab
+	;;
 
-\
-    registry)
-    docker run -d --name registry \
-        --net network --restart unless-stopped \
-        -v /Volumes/dev/DockerRegistry:/var/lib/registry \
-        -e REGISTRY_HTTP_HOST=https://nextlab.hwangsehyun.com:41443 \
-        registry
-    ;;
+registry)
+	docker run -d --name registry \
+		--net network --restart unless-stopped \
+		-v /Volumes/dev/DockerRegistry:/var/lib/registry \
+		-e REGISTRY_HTTP_HOST=https://nextlab.hwangsehyun.com:41443 \
+		registry
+	;;
 
-\
-    docker-hub)
-    docker run -d --name docker-hub \
-        --restart unless-stopped \
-        -p 5000:5000 -v /Volumes/dev/DockerHub:/var/lib/registry \
-        -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io \
-        registry
-    ;;
+docker-hub)
+	docker run -d --name docker-hub \
+		--restart unless-stopped \
+		-p 5000:5000 -v /Volumes/dev/DockerHub:/var/lib/registry \
+		-e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io \
+		registry
+	;;
 
-\
-    mongo)
-    Password
-    docker run -d --name mongo \
-        -w /root -v /Volumes/dev/Mongo:/data/db \
-        -p 27017:27017 \
-        -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=$REPLY \
-        mongo
-    ;;
+mongo)
+	Password
+	docker run -d --name mongo \
+		-w /root -v /Volumes/dev/Mongo:/data/db \
+		-p 27017:27017 \
+		-e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=$REPLY \
+		mongo
+	;;
 
-\
-    mysql)
-    Password
-    docker run --name mysql -d \
-        --restart always \
-        -v /Volumes/dev/MySQL:/var/lib/mysql \
-        -e MYSQL_ROOT_PASSWORD=$PASSWORD \
-        -p 3306:3306 mysql
-    ;;
+mysql)
+	Password
+	docker run --name mysql -d \
+		--restart always \
+		-v /Volumes/dev/MySQL:/var/lib/mysql \
+		-e MYSQL_ROOT_PASSWORD=$PASSWORD \
+		-p 3306:3306 mysql
+	;;
 
-\
-    boilerpipe)
-    docker run --rm \
-        -p 8000:80 \
-        tasubo/boilerpipe-api
-    ;;
+boilerpipe)
+	docker run --rm \
+		-p 8000:80 \
+		tasubo/boilerpipe-api
+	;;
 
-\
-    cache)
-    docker run -d --name cache \
-        --restart unless-stopped \
-        -p 3141:3141 --net network \
-        -v /Volumes/dev/pypi:/var/cache/nginx/pypi \
-        -v /Volumes/dev/nginx.conf:/etc/nginx/nginx.conf \
-        nginx:alpine
-    ;;
+cache)
+	docker run -d --name cache \
+		--restart unless-stopped \
+		-p 3141:3141 --net network \
+		-v /Volumes/dev/pypi:/var/cache/nginx/pypi \
+		-v /Volumes/dev/nginx.conf:/etc/nginx/nginx.conf \
+		nginx:alpine
+	;;
 
-\
-    apt)
-    docker run -d --name apt --init \
-        --restart unless-stopped \
-        -p 3142:3142 --net network \
-        -w /root -e CACHE=localhost \
-        -v /Volumes/dev/apt:/var/cache/apt-cacher-ng \
-        jrcichra/apt-cacher-ng
-    ;;
+apt)
+	docker run -d --name apt --init \
+		--restart unless-stopped \
+		-p 3142:3142 --net network \
+		-w /root -e CACHE=localhost \
+		-v /Volumes/dev/apt:/var/cache/apt-cacher-ng \
+		jrcichra/apt-cacher-ng
+	;;
 
-\
-    *)
-    echo $1 is not a valid container name
-    ;;
+*)
+	echo $1 is not a valid container name
+	;;
 esac
 
 docker logs --tail 100 $1
